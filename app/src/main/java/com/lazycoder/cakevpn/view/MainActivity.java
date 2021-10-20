6936bce6905bfd0c66e70df12b16742e39aeddc5
tinycellstuan <89169192+tinycellstuan@users.noreply.github.com>
2021-10-20 11:47:39 -0500

    Update MainActivity.java
    Update code

--- app/src/main/java/com/lazycoder/cakevpn/view/MainActivity.java
+++ app/src/main/java/com/lazycoder/cakevpn/view/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
+import androidx.databinding.DataBindingUtil;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
